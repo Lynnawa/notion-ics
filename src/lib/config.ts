6a6adb09bc7a43c6accbcb57f38f11e3fc76dfef
Lynnawa <qfffffflsl@gmail.com>
2026-03-05 13:21:46 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,13 +4,10 @@ import type { QueryDataSourceParameters } from '@notionhq/client/build/src/api-e
 export default {
 	filter: {
 		and: [
-			{ property: 'Status', status: { does_not_equal: 'Completed' } },
-			{ property: 'Status', status: { does_not_equal: 'Nope' } },
-			{ property: 'Type', select: { equals: 'Task' } }
 		]
 	},
-	dateProperty: 'Scheduled',
-	titleProperty: 'Name',
+	dateProperty: '日期',
+	titleProperty: '名称',
 	busy: ICalEventBusyStatus.FREE
 } as {
 	filter: Readonly<QueryDataSourceParameters['filter']>;
